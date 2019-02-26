8fcfdeaf1775263aaf1f55d55cc85b8fb3b3f130
Nguyen Doan Hien <toilati123vn@gmail.com>
2019-02-26 12:12:05 +0700

    update

--- QLBH/admin/SanPhamNhom.aspx.designer.cs
+++ QLBH/admin/SanPhamNhom.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace QLBH.admin {
         protected global::System.Web.UI.WebControls.GridView GridView1;
         
         /// <summary>
+        /// DetailsView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DetailsView DetailsView1;
+        
+        /// <summary>
         /// EntityDataSource1 control.
         /// </summary>
         /// <remarks>
